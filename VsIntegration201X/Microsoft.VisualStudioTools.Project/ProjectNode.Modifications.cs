4ceb017de00606d1c0773127162dc814d06b4ef0
Stitchous <alexander.amrilloev@gmail.com>
2013-11-22 19:57:28 +0400

    configuration dependent property pages implemented

---
+++ VsIntegration201X/Microsoft.VisualStudioTools.Project/ProjectNode.Modifications.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.VisualStudioTools.Project
+{
+    partial class ProjectNode
+    {
+        protected internal void SetOpened()
+        {
+            this.projectOpened = true;
+        }
+
+        protected internal void SetIsDirty(bool isDirty)
+        {
+            this.isDirty = isDirty;
+        }
+    }
+}
